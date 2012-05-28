3bcf485ceb06715123691ff89e287b942686cef9
Fred Sauer <fredsa@google.com>
2012-05-27 17:39:07 -0700

    Show date at top of chore list

--- src/sauer/motivate/MotivateMainActivity.java
+++ src/sauer/motivate/MotivateMainActivity.java
@@ -1,27 +1,36 @@
 package sauer.motivate;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.Message;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.ToggleButton;
 
 public class MotivateMainActivity extends Activity {
   LinearLayout choreLinearLayout;
 
+  Date choreDate;
+  DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.FULL);
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    choreDate = new Date();
+
     setContentView(R.layout.motivate_main);
     choreLinearLayout = (LinearLayout) findViewById(R.id.chore_list_linear_layout);
 
-    final TextView addChoreTextView = (TextView) findViewById(R.id.add_chore_textview);
+    TextView dayDescriptionTextView = (TextView) findViewById(R.id.day_description_text_view);
+    dayDescriptionTextView.setText(dateFormat.format(choreDate));
+
+    final TextView addChoreTextView = (TextView) findViewById(R.id.add_chore_text_view);
     addChoreTextView.setOnClickListener(new OnClickListener() {
       @Override
       public void onClick(View v) {
